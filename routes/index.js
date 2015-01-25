2c23a166b3e528cfdb536f108729971a237da587
Scott Prue <sprue.dev@gmail.com>
2015-01-25 03:14:07 -0500

    Exec added for running unix commands. Function for running grunt tasks was added.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var shell = require('shelljs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Daux Server' });
 });
-
+router.post('/run', function(req, res, next){
+  runGruntTask('', function(){
+    res.end();
+  });
+});
+function runGruntTask(task, cb) {
+  var taskCommand = "grunt" + task;
+  shell.exec(task, function(){
+    console.log(task + ' was run successfully.');
+    if(cb){
+      cb();
+    }
+  });
+}
 module.exports = router;
